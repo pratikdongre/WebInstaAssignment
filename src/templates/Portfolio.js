794addb1dc65db63af7282b488ba8312966f7762
pratik dongre <pratikdon2001@gmail.com>
2025-04-15 20:51:09 +0530

    position updates on Templates

--- src/templates/Portfolio.js
+++ src/templates/Portfolio.js
@@ -2,28 +2,40 @@ export default [
   {
     id: "text-1",
     type: "text",
-    content: "Welcome, To my Portfolio!",
+    content: "Up in the sky",
     x: 50,
     y: 40,
-    width: 300,
-    height: 50,
+    width: 282,
+    height: 32,
   },
+  {
+    id: "text-2",
+    type: "text",
+    content:
+      "Hey there! Just wanted to share some good vibes about what's happening up in the sky! The stars are aligning in such a way that it feels good to just gaze up and get lost in the vastness.",
+    x: 50,
+    y: 40,
+    width: 282,
+    height: 204,
+  },
+
   {
     id: "button-1",
     type: "button",
     content: "Click to Contact",
     x: 50,
     y: 120,
-    width: 200,
-    height: 50,
+    width: 182,
+    height: 32,
   },
   {
     id: "image-1",
     type: "image",
-    content: "https://via.placeholder.com/150", // Replace with your actual image
-    x: 50,
-    y: 200,
-    width: 150,
-    height: 150,
+    content:
+      "https://gamesrawg.websites.co.in/files/2260488/updates/3063380/up-in-the-sky_17440915115ZogeQ.jpeg", // Replace with your actual image
+    x: 300,
+    y: 100,
+    width: 270,
+    height: 316,
   },
 ];
